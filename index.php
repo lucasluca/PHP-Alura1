e0a163677e88a740973f54fec28dd529ba361c0b
AionAngel <lucas.augustus@gmail.com>
2018-01-26 15:29:24 -0200

    fim da aula 1 curso de PHP alura

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
-<?php include("cabecalho.php") ?>
+<?php 
+include("cabecalho.php");
+
+if(isset($_GET['login']) && $_GET['login'] == 1) {
+    echo "<p class='alert-success'>Logado com sucesso</p>";
+}
+
+if(isset($_GET['login']) && $_GET['login'] == 0) {
+    echo "<p class='alert-danger'>Usuario ou senha Invalidos!</p>";
+}
+
+?>
 
     <h1>Bem Vindo a Loja</h1>
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+        <table class="table"> 
+        <tr>
+            <td>Email</td>
+            <td><input class="form-control" type="text" name="email"></td>
+        </tr>
+        <tr>
+            <td>Senha</td>
+            <td><input class="form-control" type="password" name="senha"></td>
+        </tr>
+        <tr>
+            <td><button class="btn btn-primary">Login</button></td>
+        </tr>
+        
+        </table>
+    </form> 
  
 <?php include("rodape.php") ?>
---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+include("conecta.php");
+include("banco-usuario.php"); 
+
+$usuario = buscaUsuario($conexao,  $_POST['email'], $_POST['senha']);
+//print_r($usuario);
+
+if($usuario == null) {
+    header("Location: index.php?login=0");
+}else {
+    setcookie("usuario_logado", $usuario['email']);
+    header("Location: index.php?login=1");
+}
+die();
